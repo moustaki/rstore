fdafb7702e3fe8762b96c542a13486d2c805a078
Yves Raimond <yves.raimond@bbc.co.uk>
2010-04-23 16:00:31 +0100

    Support for querying types and langs

--- src/rdb_entailment.pl
+++ src/rdb_entailment.pl
@@ -22,32 +22,48 @@ rdf(S, P, O) :-
     expand(SP, P),
     expand(SO, O).
 
-rdb(S, P, literal(L)) :-
+rdb(S, P, literal(type(T, V))) :-
     \+ (P == 'rdf:type'),
     table_column(P, C),
     C = 'value',
-    sformat(SQL, 'SELECT t.uri, p.value  FROM `~w` p INNER JOIN `owl:Thing` t on t.id = p.subject_id', [P]),
-    %write(SQL), write('\n'),
+    sformat(SQL, 'SELECT t.uri, p.value, p.xsd_type FROM `~w` p INNER JOIN `owl:Thing` t ON t.id = p.subject_id WHERE p.xsd_type IS NOT NULL', [P]),
+    write(SQL), write('\n'),
+    query(SQL, row(S, V, T)).
+rdb(S, P, literal(lang(L, V))) :-
+    \+ (P == 'rdf:type'),
+    table_column(P, C),
+    C = 'value',
+    sformat(SQL, 'SELECT t.uri, p.value, p.lang FROM `~w` p INNER JOIN `owl:Thing` t ON t.id = p.subject_id WHERE p.lang IS NOT NULL', [P]),
+    write(SQL), write('\n'),
+    query(SQL, row(S, V, L)).
+rdb(S, P, literal(L)) :-
+    \+ (P == 'rdf:type'),
+    \+ (L == lang(_, _)),
+    \+ (L == type(_, _)),
+    table_column(P, C),
+    C = 'value',
+    sformat(SQL, 'SELECT t.uri, p.value  FROM `~w` p INNER JOIN `owl:Thing` t ON t.id = p.subject_id WHERE p.lang IS NULL AND p.xsd_type IS NULL', [P]),
+    write(SQL), write('\n'),
     query(SQL, row(S, L)).
 rdb(S, P, O) :-
     \+ (P == 'rdf:type'),
     table_column(P, C),
     C = 'object_id',
     sformat(SQL, 'SELECT s.uri, o.uri FROM `~w` p INNER JOIN `owl:Thing` s ON s.id = p.subject_id INNER JOIN `owl:Thing` o ON o.id = p.object_id', [P]),
-    %write(SQL), write('\n'),
+    write(SQL), write('\n'),
     query(SQL, row(S, O)).
 rdb(S, 'rdf:type', O) :-
     atomic(S), atomic(O), !,
     table(O),
     sformat(SQL, 'SELECT `uri` FROM `~w` WHERE `uri` = \'~w\'', [O, S]),
-    %write(SQL), write('\n'),
+    write(SQL), write('\n'),
     query(SQL, row(S)).
 rdb(S, 'rdf:type', O) :-
     var(S), !,
     table_column(O, C), % not sure why backtracking here fails
     C = 'uri',
     sformat(SQL, 'SELECT `uri` FROM `~w`', [O]),
-    %write(SQL), write('\n'),
+    write(SQL), write('\n'),
     query(SQL, row(S)).
 
 
